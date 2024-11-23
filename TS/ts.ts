c200d4396fc969cfabad0ccc5ed345852793756f
LIHUA919 <1017343802@qq.com>
2024-11-23 12:58:18 +0800

    llama3.2 90b tesing

---
+++ TS/ts.ts
@@ -0,0 +1,104 @@
+// 162
+function findPeakElement(nums: number[]): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+
+
+
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+
+
+function searchRange(nums: number[], target: number): number[] {
+    const result = [-1, -1];
+
+    if (nums.length === 0) {
+        return result;
+    }
+
+    const leftIndex = findLeftIndex(nums, target);
+    if (leftIndex === -1) {
+        return result;
+    }
+
+    result[0] = leftIndex;
+    result[1] = findRightIndex(nums, target);
+
+    return result;
+}
+
+function findLeftIndex(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    if (left < nums.length && nums[left] === target) {
+        return left;
+    }
+
+    return -1;
+}
+
+
+function findMin(nums: number[]): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
