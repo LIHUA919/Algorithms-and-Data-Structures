c592d024046f336ca251835f71e612f670a775eb
LIHUA919 <1017343802@qq.com>
2024-11-25 19:09:50 +0800

    understanding JS

--- JS/JS.js
+++ JS/JS.js
@@ -1292,3 +1292,51 @@ function longestPalindrome(s) {
 const s = "babad";
 console.log(longestPalindrome(s));  // Output: "bab" or "aba"
 
+
+
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    const m = obstacleGrid.length;
+    const n = obstacleGrid[0].length;
+    
+    // If the start or end is an obstacle, return 0
+    if (obstacleGrid[0][0] === 1 || obstacleGrid[m - 1][n - 1] === 1) {
+        return 0;
+    }
+    
+    // Create a 2D DP array to store the number of ways to reach each cell
+    const dp = Array.from({ length: m }, () => Array(n).fill(0));
+    
+    // Start position
+    dp[0][0] = 1;
+    
+    // Fill the DP table
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0; // Obstacle cell
+            } else {
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j]; // From top
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1]; // From left
+                }
+            }
+        }
+    }
+    
+    // The answer is in the bottom-right corner
+    return dp[m - 1][n - 1];
+};
+
+// Example usage
+const grid = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+];
+console.log(uniquePathsWithObstacles(grid));  // Output: 2
