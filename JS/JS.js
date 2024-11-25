b7d11a1fd51fac3ba1b60c90564ec738304397d3
LIHUA919 <1017343802@qq.com>
2024-11-25 13:04:38 +0800

    understanding JS

--- JS/JS.js
+++ JS/JS.js
@@ -1073,3 +1073,28 @@ var combine = function(n, k) {
     backtrack(1, []);
     return result;
 };
+
+
+
+var maximalSquare = function(matrix) {
+    if (matrix.length === 0) return 0;
+
+    let rows = matrix.length, cols = matrix[0].length;
+    let dp = Array(rows).fill(0).map(() => Array(cols).fill(0));
+    let maxSquareLength = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (matrix[i][j] === '1') {
+                if (i === 0 || j === 0) {
+                    dp[i][j] = 1; // For first row or column
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1;
+                }
+                maxSquareLength = Math.max(maxSquareLength, dp[i][j]);
+            }
+        }
+    }
+
+    return maxSquareLength * maxSquareLength;
+};
