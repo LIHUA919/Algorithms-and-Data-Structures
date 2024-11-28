e8236c91311ba278d7ee7cbf4adcbdd15578810a
LIHUA919 <1017343802@qq.com>
2024-11-28 23:55:01 +0800

    123

--- python/Python.py
+++ python/Python.py
@@ -2527,7 +2527,7 @@ class Solution:
 
 
 
-        
+
         for row in matrix:
             for col in range(cols):
                 # Update heights
@@ -2537,5 +2537,27 @@ class Solution:
             max_area = max(max_area, largestRectangleArea(heights))
         
         return max_area
+
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n = len(nums)
+        val = [1] + nums + [1]
+        
+        @lru_cache(None)
+        def solve(left: int, right: int) -> int:
+            if left >= right - 1:
+                return 0
+            
+            best = 0
+            for i in range(left + 1, right):
+                total = val[left] * val[i] * val[right]
+                total += solve(left, i) + solve(i, right)
+                best = max(best, total)
+            
+            return best
+
+        return solve(0, n + 1)
+
 
 
