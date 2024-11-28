75922f1d3a2bf6693c9b6755af6876fc47594c30
LIHUA919 <1017343802@qq.com>
2024-11-28 23:57:04 +0800

    123

--- python/Python.py
+++ python/Python.py
@@ -2559,5 +2559,14 @@ class Solution:
 
         return solve(0, n + 1)
 
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        count = 0
+        for factor in range(1, n+1):
+            if n % factor == 0:
+                count += 1
+                if count == k:
+                    return factor
+        return -1
 
 
