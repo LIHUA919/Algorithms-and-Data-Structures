81310582f7a7654391ac3b2cdf1d4571a77729d1
LIHUA919 <1017343802@qq.com>
2024-11-28 23:57:43 +0800

    123

--- python/Python.py
+++ python/Python.py
@@ -2570,3 +2570,11 @@ class Solution:
         return -1
 
 
+
+import math
+
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        mx, mn = max(nums), min(nums)
+        return math.gcd(mx, mn)
+
