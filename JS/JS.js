c152d0fe682aead8512b6a034cb708b5eb50a532
LIHUA919 <1017343802@qq.com>
2024-12-16 00:16:57 +0800

    testing

--- JS/JS.js
+++ JS/JS.js
@@ -1575,4 +1575,26 @@ var countPrimes = function(n) {
     return ans;
 };
 
+var findCircleNum = function(isConnected) {
+    const cities = isConnected.length;
+    const visited = new Set();
+    let provinces = 0;
+    for (let i = 0; i < cities; i++) {
+        if (!visited.has(i)) {
+            dfs(isConnected, visited, cities, i);
+            provinces++;
+        }
+    }
+    return provinces;
+};
+
+const dfs = (isConnected, visited, cities, i) => {
+    for (let j = 0; j < cities; j++) {
+        if (isConnected[i][j] == 1 && !visited.has(j)) {
+            visited.add(j);
+            dfs(isConnected, visited, cities, j);
+        }
+    }
+};
+
 
