fea701c6d5e486f9a5729ad8d65d060df6e72077
LIHUA919 <1017343802@qq.com>
2024-12-16 00:35:01 +0800

    testing

--- python/Python.py
+++ python/Python.py
@@ -3724,3 +3724,25 @@ class Solution:
         
         return total(0, len(nums) - 1, 1) >= 0
 
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        factorial = [1]
+        for i in range(1, n):
+            factorial.append(factorial[-1] * i)
+        
+        k -= 1
+        ans = list()
+        valid = [1] * (n + 1)
+        for i in range(1, n + 1):
+            order = k // factorial[n - i] + 1
+            for j in range(1, n + 1):
+                order -= valid[j]
+                if order == 0:
+                    ans.append(str(j))
+                    valid[j] = 0
+                    break
+            k %= factorial[n - i]
+
+        return "".join(ans)
+
