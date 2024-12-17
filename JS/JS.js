8e99606d36655263fff7a086bfbd45166d7256f7
LIHUA919 <1017343802@qq.com>
2024-12-17 14:40:57 +0800

    testing

--- JS/JS.js
+++ JS/JS.js
@@ -1764,3 +1764,22 @@ var canPartition = function(nums) {
     return dp[n - 1][target];
 };
 
+
+
+const isPrime = (x) => {
+    for (let i = 2; i * i <= x; ++i) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var countPrimes = function(n) {
+    let ans = 0;
+    for (let i = 2; i < n; ++i) {
+        ans += isPrime(i);
+    }
+    return ans;
+};
+
