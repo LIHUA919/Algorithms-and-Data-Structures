4eaec913bc56a90d07179bf76ab37fded8184a82
LIHUA919 <1017343802@qq.com>
2024-12-17 14:39:46 +0800

    testing git commit -m testing

--- python/Python.py
+++ python/Python.py
@@ -3975,3 +3975,17 @@ class Solution:
             for j in range(columns):
                 ans = max(ans, dfs(i, j))
         return ans
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
