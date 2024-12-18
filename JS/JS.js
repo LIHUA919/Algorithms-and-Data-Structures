5a3f06ea9b720f60c1cf990e2ed61beecbe0a400
LIHUA919 <1017343802@qq.com>
2024-12-18 14:08:07 +0800

    testing

--- JS/JS.js
+++ JS/JS.js
@@ -1986,7 +1986,16 @@ var waysToFillArray = function(queries) {
 };
 
 
-  
+
+var kthFactor = function(n, k) {
+    const arr = [];
+    for(let i = 1; i <= n/2; i++){
+        if(n % i == 0) arr.push(i);
+    }
+    arr.push(n);
+    return arr[k-1] || -1;
+};
+
   
   
   
