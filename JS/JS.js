14d0d62d04fc7a2a9d59e87da0347a424c220882
LIHUA919 <1017343802@qq.com>
2024-12-18 14:16:52 +0800

    testing

--- JS/JS.js
+++ JS/JS.js
@@ -2154,3 +2154,46 @@ var lengthOfLIS = function(nums) {
     }
     return dp[n];
 };
+
+
+
+var maximumDetonation = function(bombs) {
+    const n = bombs.length;
+    // 判断炸弹 u 能否引爆炸弹 v
+    const isConnected = (u, v) => {
+        const dx = bombs[u][0] - bombs[v][0];
+        const dy = bombs[u][1] - bombs[v][1];
+        return bombs[u][2] * bombs[u][2] >= dx * dx + dy * dy;
+    };
+    
+    // 维护引爆关系有向图
+    const edges = new Map();
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (i !== j && isConnected(i, j)) {
+                if (!edges.has(i)) edges.set(i, []);
+                edges.get(i).push(j);
+            }
+        }
+    }
+    let res = 0; // 最多引爆数量
+    for (let i = 0; i < n; ++i) {
+        // 遍历每个炸弹，广度优先搜索计算该炸弹可引爆的数量，并维护最大值
+        const visited = Array(n).fill(0);
+        let cnt = 1;
+        const q = [i];
+        visited[i] = 1;
+        while (q.length > 0) {
+            const cidx = q.shift();
+            for (const nidx of edges.get(cidx) || []) {
+                if (visited[nidx]) continue;
+                ++cnt;
+                q.push(nidx);
+                visited[nidx] = 1;
+            }
+        }
+        res = Math.max(res, cnt);
+    }
+    return res;
+};
+
