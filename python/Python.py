736aaf3358de878a77ef26e7f5087bcba53c1fae
LIHUA919 <1017343802@qq.com>
2024-12-19 11:46:10 +0800

    testing

--- python/Python.py
+++ python/Python.py
@@ -4019,3 +4019,20 @@ class Solution:
         dfs([])
         return res
 
+
+
+class Solution:
+    def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
+        ans = 0
+        m, n = len(grid), len(grid[0])
+        s = [[[0, 0] for _ in range(n + 1)] for _ in range(m + 1)]
+        for i, row in enumerate(grid):
+            for j, c in enumerate(row):
+                s[i + 1][j + 1][0] = s[i + 1][j][0] + s[i][j + 1][0] - s[i][j][0]
+                s[i + 1][j + 1][1] = s[i + 1][j][1] + s[i][j + 1][1] - s[i][j][1]
+                if c != '.':
+                    s[i + 1][j + 1][ord(c) & 1] += 1
+                if s[i + 1][j + 1][0] and s[i + 1][j + 1][0] == s[i + 1][j + 1][1]:
+                    ans += 1
+        return ans
+
