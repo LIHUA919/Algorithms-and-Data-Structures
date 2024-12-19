47d0244b4d20037906cb7c9a3d8eabf8e5837ca3
LIHUA919 <1017343802@qq.com>
2024-12-19 11:44:37 +0800

    testing

--- python/Python.py
+++ python/Python.py
@@ -4000,3 +4000,22 @@ class Solution:
             ans += bisect_right(nums, k, 0, i + 1) - bisect_left(nums, k, 0, i + 1)
         return ans
 
+
+
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        res = []
+        def dfs(arr: List[str]) -> None:
+            if len(arr) == n:
+                res.append(''.join(arr))
+            else:
+                if arr == [] or arr[-1] == '1':
+                    arr.append('0')
+                    dfs(arr)
+                    arr.pop()
+                arr.append('1')
+                dfs(arr)
+                arr.pop()
+        dfs([])
+        return res
+
